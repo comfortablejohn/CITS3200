2ddf0831698b4798a70cfaff75142e72cc25a688
tsujp <tsujp@users.noreply.github.com>
2014-08-20 12:35:05 +0800

    Site Layout Done
    Rough site layout complete.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,17 +2,17 @@ Rails.application.configure do
 
   # START
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  # TURN ON config.assets.compress = true
 
   # Choose the compressors to use
   # config.assets.js_compressor  = :uglifier
   # config.assets.css_compressor = :yui
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  # TURN ON config.assets.compile = false
 
   # Generate digests for assets URLs.
-  config.assets.digest = true
+  # TURN ON config.assets.digest = true
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
